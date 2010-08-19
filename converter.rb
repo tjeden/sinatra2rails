0200ce832792db7bb6e2f25e58d1fccd993fc27c
Aleksander Dabrowski <aleks@kumulator.pl>
2010-08-19 19:22:58 +0200

    first cleaning

--- converter.rb
+++ converter.rb
@@ -6,25 +6,40 @@ require 'ruby2ruby'
 
 require 'pp'
 
-$found = []
-def find_entities(entity, detect=nil, detect2=nil)
-  ruby2ruby = Ruby2Ruby.new
-  if(entity.is_a?(Array) && (entity[0] == detect) && (entity[2] == detect2))
-    $found << entity
-    puts "---"
-    puts ruby2ruby.process(entity[3])
-  elsif(entity.is_a?(Array))
-    entity.each{|e| find_entities(e, detect, detect2)}
-  else
-    #fuck it
+class EntityFinder
+  attr_accessor :found
+  
+  def initialize
+    @found = []
+  end
+
+  def find_entities(entity, detect=nil, detect2=nil)
+    if(entity.is_a?(Array) && (entity[0] == detect) && (entity[2] == detect2))
+      @found << entity
+    elsif(entity.is_a?(Array))
+      entity.each{|e| find_entities(e, detect, detect2)}
+    end
+    @found
   end
 end
 
-pt = ParseTree.new
-ruby2ruby = Ruby2Ruby.new
+class Sinatra2Rails
+  attr_accessor :file_name
+  
+  def initialize( file_name)
+    @file_name = file_name
+    pt = ParseTree.new
+    ruby2ruby = Ruby2Ruby.new
 
-sexp_array = pt.process(File.open('pong.rb').read)
+    sexp_array = pt.process(File.open(@file_name).read)
 
-find_entities(sexp_array, :iter)
+    finder = EntityFinder.new
+    finder.find_entities(sexp_array, :iter).each do |elem|
+      puts "---"
+      puts ruby2ruby.process(elem[3])
+    end
+  end
+end
 
-pp $found
+Sinatra2Rails.new('pong.rb')
+
